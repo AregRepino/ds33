1e7fd3b55fec37841333fe5981d8181b60c314f2
areg <test@gmail.com>
2022-03-26 21:49:43 +0300

    fix

--- src/main/java/com/example/D/S3/BotController.java
+++ src/main/java/com/example/D/S3/BotController.java
@@ -1,15 +1,16 @@
 package com.example.D.S3;
 
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class BotController {
-//
-//
-//    @GetMapping("/hello")
-//    public String hello () {
-//        return "hello";
-//    }
-//
-//}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BotController {
+
+
+    @GetMapping("/hello")
+    public String hello () {
+        return "hello";
+    }
+
+
+}
